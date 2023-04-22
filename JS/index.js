c1c64ce73fbaa23e97c44e8f31d2ec7135b82b18
Federico Stempler <zackmango@gmail.com>
2023-04-22 17:46:09 -0300

    Main Ok / While Ok / Switch Ok

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,12 @@
+
 //Función para elegir y recomendar disco de jazz
 function jazzRecord(){
-  let jazzrecord = prompt(`You chose ${genre}! Please, tell us your favourite ${genre} record`)
-  if (jazzrecord == "") {
-    alert ("We don't know that record, please tell us another so we can recommend you a new you'll enjoy")
+  let recordName = prompt(`You chose Jazz! Please, tell us your favourite Jazz record`)
+  if (recordName == "") {
+    alert ("We don't know that record, please tell us another so we can recommend you a new one you'll enjoy")
     jazzRecord()
   }else{
-    if(jazzrecord == "Miles Davis - Kind of Blue"){
+    if(recordName == "Miles Davis - Kind of Blue"){
     alert ("Great record! We recommend you: John Coltrane - A Love Supreme \nhttps://open.spotify.com/album/4o1ObbFgVBo6rxIdW2vnxM?si=xvvV9-I9QJShZjNMKrlSqw" )
   } else{
     alert ("Great record! We recommend you: Miles Davis - Kind of Blue \nhttps://open.spotify.com/album/1weenld61qoidwYuZ1GESA?si=QwW8wHRCQbWfBGRbR4MfKw")
@@ -14,12 +15,12 @@ function jazzRecord(){
 }
 //Función para elegir y recomendar disco de rock
 function rockRecord(){
-  let rockrecord = prompt(`You chose ${genre}! Please, tell us your favourite ${genre} record`)
-  if (rockrecord == "") {
+  let recordName = prompt(`You chose ${genre}! Please, tell us your favourite ${genre} record`)
+  if (recordName == "") {
     alert ("We don't know that record, please tell us another so we can recommend you a new one you'll enjoy")
     rockRecord()
   }else{
-    if(rockrecord == "Led Zepellin - IV"){
+    if(recordName == "Led Zepellin - IV"){
     alert ("Great record! We recommend you: The Police - Outlandos D'Amour \nhttps://open.spotify.com/album/1H9g6j4Wwj6wh6p8YHVtkf?si=skTyluytSfSD3sT9VtsOjA" )
   } else{
     alert ("Great record! We recommend you: Led Zeppelin - Led Zeppelin IV \nhttps://open.spotify.com/album/5EyIDBAqhnlkAHqvPRwdbX?si=IEgM9EcHRaKxfIer0L6Zog")
@@ -28,12 +29,12 @@ function rockRecord(){
 }
 //Función para elegir y recomendar disco de pop
 function popRecord(){
-  let poprecord = prompt(`You chose ${genre}! Please, tell us your favourite ${genre} record`)
-  if (poprecord == "") {
+  let recordName = prompt(`You chose ${genre}! Please, tell us your favourite ${genre} record`)
+  if (recordName == "") {
     alert ("We don't know that record, please tell us another so we can recommend you a new one you'll enjoy")
     popRecord()
   }else{
-    if(poprecord == "Michael Jackson - Thriller"){
+    if(recordName == "Michael Jackson - Thriller"){
     alert ("Great record! We recommend you: Prince - Purple Rain \nhttps://open.spotify.com/album/7nXJ5k4XgRj5OLg9m8V3zc?si=qCTsST6fSbCEhe75RsqHYQ" )
   } else{
     alert ("Great record! We recommend you: Michael Jackson - Thriller \nhttps://open.spotify.com/album/2ANVost0y2y52ema1E9xAZ?si=7tJUyun-ToaqoU55C8cYuw")
@@ -41,16 +42,10 @@ function popRecord(){
 }
 }
 
-
-
-do { 
-let name = prompt("Please enter your name:");
-alert("Hi " + name + "! Please select your favorite music genre:\n1. Jazz\n2. Rock\n3. Pop\n4. None");
-
-let genre = "";
-let genreNumber = parseInt(prompt("Enter the number of your chosen genre:"));
-
-switch(genreNumber) {
+let trueOfalse = true; 
+//Función para recomendar disco. Una vez ingresado el disco preferido recomienda uno del género con su link a Spotify. Si el disco ingresado es el que figura en la función elige la 2da opción. 
+function record(genreNumber){
+  switch (genreNumber) {
   case 1:
     genre = "Jazz";
     jazzRecord()
@@ -76,9 +71,27 @@ switch(genreNumber) {
     }
     break;
   default:
-    alert("Invalid input, please choose a number between 1 and 4.");
-    
-    break;
+  //Si no se ingresa un número válido se repite la petición hasta que se ingrese.
+    let genreNumber = parseInt(prompt("INVALID number " + name + " Please choose your favorite music genre: \n1 Jazz \n2 Rock \n3 Pop \n4 None "))
+    record(genreNumber);
+  
+  }
+
 }
-var starAgain = confirm("Do you want to start again?");
-}while (startAgain);
+
+//Se pide ingresar el nombre hasta que se ingresa
+let truOfalse = true
+let name = prompt("Please enter your name:");
+
+while (trueOfalse) {
+  if (name == "" ){
+    
+    name = prompt("Please enter your name:");
+        
+  } else {
+    let genreNumber = parseInt(prompt("Hi " + name + "! Please choose your favorite music genre: \n1 Jazz \n2 Rock \n3 Pop \n4 None "))
+    record(genreNumber);
+    trueOfalse = false; 
+  }
+}
+
